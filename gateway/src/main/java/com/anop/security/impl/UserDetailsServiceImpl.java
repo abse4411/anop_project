397b4d53a559f8abe5a5fea0488e491d74f22224
Shen Zhifeng <shen_zhifeng@outlook.com>
2020-05-23 01:17:04 +0800

    基于WebFlux验证服务实现

--- gateway/src/main/java/com/anop/security/impl/UserDetailsServiceImpl.java
+++ gateway/src/main/java/com/anop/security/impl/UserDetailsServiceImpl.java
@@ -5,31 +5,33 @@ import com.anop.security.mapper.UserMapper;
 import com.anop.security.pojo.Role;
 import com.anop.security.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 @Component
-public class UserDetailsServiceImpl implements UserDetailsService {
+public class UserDetailsServiceImpl implements ReactiveUserDetailsService {
     @Autowired
     private UserMapper userMapper;
     @Autowired
     private RoleMapper roleMapper;
 
     @Override
-    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+    public Mono<UserDetails> findByUsername(String userName) {
         //查数据库
         User user = userMapper.loadUserByUsername(userName);
         if (null != user) {
             List<Role> roles = roleMapper.getRolesByUserId(user.getId());
             user.setAuthorities(roles);
         } else {
-            throw new UsernameNotFoundException(userName);
+            return  Mono.error(new UsernameNotFoundException(userName));
         }
 
-        return user;
+        return Mono.just(user);
     }
 }
--- notification_center/src/main/java/com/anop/config/SecurityConfig.java
+++ notification_center/src/main/java/com/anop/config/SecurityConfig.java
@@ -12,6 +12,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic().disable();
+        //http.csrf().disable();
         http.authorizeRequests().anyRequest().authenticated();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
     }
