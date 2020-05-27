780bffadf21cd3ada4d2aadea84874373fbee33e
Shen Zhifeng <shen_zhifeng@outlook.com>
2020-05-28 00:54:59 +0800

    工具类模块：新增用于集合类型映射的方法

--- utils/src/main/java/com/anop/util/PropertyMapperUtils.java
+++ utils/src/main/java/com/anop/util/PropertyMapperUtils.java
@@ -5,6 +5,9 @@ import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.regex.Pattern;
 
 /**
@@ -17,6 +20,7 @@ public class PropertyMapperUtils {
     private static final Pattern GET_PATTERN = Pattern.compile("get\\w+");
     private static final String GET = "get";
     private static final String SET = "set";
+    private static final int COMMON_PUBLIC_METHOD_AMOUNT = 9;
 
     public static void map(Object source, Object target) {
         if (source == null || target == null) {
@@ -41,6 +45,47 @@ public class PropertyMapperUtils {
         }
     }
 
+    public static <S, T> Collection<T> map(Collection<S> source, Class<S> sourceClass, Class<T> targetClass) {
+        if (sourceClass == null || targetClass == null) {
+            throw new NullPointerException("sourceClass or targetClass must not be null");
+        }
+        if (source == null) {
+            throw new NullPointerException("source or target must not be null");
+        }
+        Collection<T> target = new ArrayList<>(source.size());
+        if (source.size() < 1) {
+            return target;
+        }
+        Method[] methods = sourceClass.getMethods();
+        List<Method> getMethods = new ArrayList<>(methods.length - COMMON_PUBLIC_METHOD_AMOUNT);
+        List<Method> setMethods = new ArrayList<>(methods.length - COMMON_PUBLIC_METHOD_AMOUNT);
+        for (int i = 0; i < methods.length; i++) {
+            if (GET_PATTERN.matcher(methods[i].getName()).matches()) {
+                String setMethodName = methods[i].getName().replace(GET, SET);
+                try {
+                    Method setMethod = targetClass.getMethod(setMethodName, methods[i].getReturnType());
+                    getMethods.add(methods[i]);
+                    setMethods.add(setMethod);
+                } catch (NoSuchMethodException e) {
+                    LOGGER.info("mapping failed:" + e.getMessage());
+                }
+            }
+        }
+        for (S item : source) {
+            try {
+                T instance = targetClass.newInstance();
+                for (int i = 0; i < getMethods.size(); i++) {
+                    setMethods.get(i).invoke(instance, getMethods.get(i).invoke(item));
+                }
+                target.add(instance);
+            } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                LOGGER.info("can not create instance of " + e.getMessage());
+                return null;
+            }
+        }
+        return target;
+    }
+
     public static <T> T map(Object source, Class<T> targetClass) {
         if (targetClass == null) {
             throw new NullPointerException("targetClass must not be null");
@@ -49,7 +94,7 @@ public class PropertyMapperUtils {
         try {
             target = targetClass.newInstance();
         } catch (InstantiationException | IllegalAccessException e) {
-            LOGGER.info("can not create instance of" + e.getMessage());
+            LOGGER.info("can not create instance of " + e.getMessage());
             return null;
         }
         map(source, target);
