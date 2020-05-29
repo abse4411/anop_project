43870341e6da0edb9524f5a0e5e0347b21340838
Shen Zhifeng <shen_zhifeng@outlook.com>
2020-05-29 16:09:29 +0800

    修复测试时登录用户获取失败问题

--- utils/src/main/java/com/anop/util/SecurityUtils.java
+++ utils/src/main/java/com/anop/util/SecurityUtils.java
@@ -2,6 +2,7 @@ package com.anop.util;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 
 /**
  * 登录用户帮助工具
@@ -9,7 +10,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
  * @author Xue_Feng
  */
 public class SecurityUtils {
-    public static <T> T getLoginUser(Class<T> tClass) {
+    public static <T extends UserDetails> T getLoginUser(Class<T> tClass) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         Object principal = authentication.getPrincipal();
         if (principal.getClass().equals(tClass)) {
