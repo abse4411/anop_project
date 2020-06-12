8f01d5e2e3a809887fb0a23f1c7b66bd24d6f265
chcic <1778206149@qq.com>
2020-06-12 09:54:38 +0800

    补充邮件提醒缺失配置

--- user_center/src/main/java/com/anop/UserCenterApplication.java
+++ user_center/src/main/java/com/anop/UserCenterApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
+@EnableScheduling
 @MapperScan("com.anop.mapper")
 public class UserCenterApplication {
     public static void main(String[] args) {
